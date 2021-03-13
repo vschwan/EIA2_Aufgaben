95e8f56adb5f2f877b97276008f365d9a04990fd
Valentina Schwan <56428408+vschwan@users.noreply.github.com>
2021-03-13 17:39:41 +0100

    test

---
+++ L00/zweitertest.js
@@ -0,0 +1,5 @@
+"use strict";
+let var1 = 5;
+let var2 = 10;
+console.log(var2 + var1);
+//# sourceMappingURL=zweitertest.js.map
---
+++ L00/zweitertest.ts
@@ -0,0 +1,3 @@
+let var1: number = 5;
+let var2: number = 10;
+console.log(var2 + var1);
