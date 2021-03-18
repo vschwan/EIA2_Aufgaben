ba31906d506f235f7e13f83551da60852b668a24
Valentina Schwan <56428408+vschwan@users.noreply.github.com>
2021-03-18 13:04:44 +0100

    another commit

---
+++ L02/l02.js
@@ -0,0 +1,9 @@
+"use strict";
+var L02;
+(function (L02) {
+    window.addEventListener("load", handleLoad);
+    function handleLoad(_event) {
+        console.log(_event);
+    }
+})(L02 || (L02 = {}));
+//# sourceMappingURL=l02.js.map
---
+++ L02/l02.ts
@@ -0,0 +1,8 @@
+namespace L02 {
+
+    window.addEventListener("load", handleLoad);
+    function handleLoad(_event: Event): void {
+        console.log(_event);
+    }
+
+}
