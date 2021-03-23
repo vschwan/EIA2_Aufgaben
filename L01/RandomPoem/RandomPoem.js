d966050977f4986422f5ba4e2ae12b33ecc7ef60
Valentina Schwan <56428408+vschwan@users.noreply.github.com>
2021-03-23 19:41:25 +0100

    another commit

--- L01/RandomPoem/RandomPoem.js
+++ L01/RandomPoem/RandomPoem.js
@@ -1,22 +1,28 @@
 "use strict";
 var RandomPoet_L02;
 (function (RandomPoet_L02) {
-    console.log("Hallo, du Muggel");
+    console.log("Hallo, du oller Hobbit");
     let subject = ["Gandalf", "Bilbo", "Legolas", "Gimli", "Gollum", "Aragorn"];
-    let predicate = ["sucht", "findet", "liebt", "hasst", "singt", "vernichtet"];
+    let predicate = ["sucht", "findet", "liebt", "hasst", "verflucht", "vernichtet"];
     let object = ["den Ring", "den Schicksalsberg", "Elben", "Sauron", "Mittelerde", "die Adler"];
-    let verse = "Elbengeflüster";
-    console.log(subject, predicate, object);
+    // console.log(subject, predicate, object);
     for (let i = 6; i <= object.length; i--) {
         if (i == 0) {
             break;
         }
-        console.log(i);
-        getVerse(subject, predicate, object, verse);
+        // console.log(i);
+        getVerse(subject, predicate, object);
     }
-    function getVerse(_subject, _predicate, _object, _verse) {
-        console.log(_verse);
-        return _verse;
+    function getVerse(_subject, _predicate, _object) {
+        let verse = "";
+        let nSubject = Math.floor(Math.random() * _subject.length);
+        verse += _subject.splice(nSubject, 1) + " ";
+        let nPredicate = Math.floor(Math.random() * _subject.length);
+        verse += _predicate.splice(nPredicate, 1) + " ";
+        let nObject = Math.floor(Math.random() * _subject.length);
+        verse += _object.splice(nObject, 1);
+        console.log(verse);
+        return verse;
     }
 })(RandomPoet_L02 || (RandomPoet_L02 = {}));
 //# sourceMappingURL=RandomPoem.js.map
--- L01/RandomPoem/RandomPoem.ts
+++ L01/RandomPoem/RandomPoem.ts
@@ -1,26 +1,38 @@
 namespace RandomPoet_L02 {
-    console.log("Hallo, du Muggel");
+    console.log("Hallo, du oller Hobbit");
 
     let subject: string[] = ["Gandalf", "Bilbo", "Legolas", "Gimli", "Gollum", "Aragorn"];
-    let predicate: string[] = ["sucht", "findet", "liebt", "hasst", "singt", "vernichtet"];
+    let predicate: string[] = ["sucht", "findet", "liebt", "hasst", "verflucht", "vernichtet"];
     let object: string[] = ["den Ring", "den Schicksalsberg", "Elben", "Sauron", "Mittelerde", "die Adler"];
-    let verse: string = "Elbengeflüster";
 
-    console.log(subject, predicate, object);
+
+    // console.log(subject, predicate, object);
 
     for (let i: number = 6; i <= object.length; i--) {
         if (i == 0) {
             break;
         }
-        console.log(i);
-        getVerse(subject, predicate, object, verse);
+        // console.log(i);
+        getVerse(subject, predicate, object);
     }
 
-    function getVerse(_subject: string[], _predicate: string[], _object: string[], _verse: string): string {
+    function getVerse(_subject: string[], _predicate: string[], _object: string[]): string {
+        let verse: string = "";
 
-        console.log(_verse);
-        return _verse;
- 
+        let nSubject: number = Math.floor(Math.random() * _subject.length);
+        verse += _subject.splice(nSubject, 1) + " ";
+
+        let nPredicate: number = Math.floor(Math.random() * _subject.length);
+        verse += _predicate.splice(nPredicate, 1) + " ";
+
+        let nObject: number = Math.floor(Math.random() * _subject.length);
+        verse += _object.splice(nObject, 1);
+
+        console.log(verse);
+        return verse;
+
     }
+
+   
 
 }
