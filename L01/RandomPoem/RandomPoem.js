bc83c791837338efe77e2eb8c1bd2c0447930f8d
Valentina Schwan <56428408+vschwan@users.noreply.github.com>
2021-03-24 07:30:37 +0100

    update

--- L01/RandomPoem/RandomPoem.js
+++ L01/RandomPoem/RandomPoem.js
@@ -5,24 +5,33 @@ var RandomPoet_L02;
     let subject = ["Gandalf", "Bilbo", "Legolas", "Gimli", "Gollum", "Aragorn"];
     let predicate = ["sucht", "findet", "liebt", "hasst", "verflucht", "vernichtet"];
     let object = ["den Ring", "den Schicksalsberg", "Elben", "Sauron", "Mittelerde", "die Adler"];
+    let verse = "";
     // console.log(subject, predicate, object);
     for (let i = 6; i <= object.length; i--) {
         if (i == 0) {
             break;
         }
         // console.log(i);
-        getVerse(subject, predicate, object);
+        getVerse(subject, predicate, object, verse);
     }
-    function getVerse(_subject, _predicate, _object) {
-        let verse = "";
+    function getVerse(_subject, _predicate, _object, _verse) {
         let nSubject = Math.floor(Math.random() * _subject.length);
-        verse += _subject.splice(nSubject, 1) + " ";
+        _verse += _subject.splice(nSubject, 1) + " ";
         let nPredicate = Math.floor(Math.random() * _subject.length);
-        verse += _predicate.splice(nPredicate, 1) + " ";
+        _verse += _predicate.splice(nPredicate, 1) + " ";
         let nObject = Math.floor(Math.random() * _subject.length);
-        verse += _object.splice(nObject, 1);
-        console.log(verse);
-        return verse;
+        _verse += _object.splice(nObject, 1);
+        console.log(_verse);
+        return _verse;
     }
+    /*FRAGEN
+    Schneide mit splice(...) ein Wort aus dem übergebenen Array von Subjekten an der Stelle der Zufallszahl heraus
+    und addiere es zur deiner Vers-Variablen. Achte darauf, das splice dir immer ein Array
+    mit den ausgeschnittenen Elementen liefert, Du musst also davon das 0-te nehmen.
+    
+    Lasse deine Vers-Variable von der Funktion zurück geben, nicht mehr einen literalen Wert
+
+    
+    */
 })(RandomPoet_L02 || (RandomPoet_L02 = {}));
 //# sourceMappingURL=RandomPoem.js.map
--- L01/RandomPoem/RandomPoem.ts
+++ L01/RandomPoem/RandomPoem.ts
@@ -4,7 +4,7 @@ namespace RandomPoet_L02 {
     let subject: string[] = ["Gandalf", "Bilbo", "Legolas", "Gimli", "Gollum", "Aragorn"];
     let predicate: string[] = ["sucht", "findet", "liebt", "hasst", "verflucht", "vernichtet"];
     let object: string[] = ["den Ring", "den Schicksalsberg", "Elben", "Sauron", "Mittelerde", "die Adler"];
-
+    let verse: string = "";
 
     // console.log(subject, predicate, object);
 
@@ -13,26 +13,40 @@ namespace RandomPoet_L02 {
             break;
         }
         // console.log(i);
-        getVerse(subject, predicate, object);
+
+        getVerse(subject, predicate, object, verse);
     }
 
-    function getVerse(_subject: string[], _predicate: string[], _object: string[]): string {
-        let verse: string = "";
+    function getVerse(_subject: string[], _predicate: string[], _object: string[], _verse: string): string {
+
 
         let nSubject: number = Math.floor(Math.random() * _subject.length);
-        verse += _subject.splice(nSubject, 1) + " ";
+        _verse += _subject.splice(nSubject, 1) + " ";
 
         let nPredicate: number = Math.floor(Math.random() * _subject.length);
-        verse += _predicate.splice(nPredicate, 1) + " ";
+        _verse += _predicate.splice(nPredicate, 1) + " ";
 
         let nObject: number = Math.floor(Math.random() * _subject.length);
-        verse += _object.splice(nObject, 1);
+        _verse += _object.splice(nObject, 1);
 
-        console.log(verse);
-        return verse;
+        console.log(_verse);
+        return _verse;
 
-    }
+    } 
+   
+
 
    
+
+    /*FRAGEN
+    Schneide mit splice(...) ein Wort aus dem übergebenen Array von Subjekten an der Stelle der Zufallszahl heraus 
+    und addiere es zur deiner Vers-Variablen. Achte darauf, das splice dir immer ein Array 
+    mit den ausgeschnittenen Elementen liefert, Du musst also davon das 0-te nehmen.
+    
+    Lasse deine Vers-Variable von der Funktion zurück geben, nicht mehr einen literalen Wert
+
+    
+    */
+
 
 }
