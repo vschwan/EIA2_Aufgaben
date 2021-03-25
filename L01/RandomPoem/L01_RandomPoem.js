0d3d9dbd37f25ba1ff0f4b1da37c5e3306923350
Valentina Schwan <56428408+vschwan@users.noreply.github.com>
2021-03-25 12:45:50 +0100

    update

--- L01/RandomPoem/L01_RandomPoem.js
+++ L01/RandomPoem/L01_RandomPoem.js
@@ -5,7 +5,6 @@ var RandomPoet_L02;
     let subject = ["Gandalf", "Bilbo", "Legolas", "Gimli", "Gollum", "Aragorn"];
     let predicate = ["sucht", "verzaubert", "liebt", "hasst", "verflucht", "vernichtet"];
     let object = ["den Ring", "den Schicksalsberg", "Elben", "Sauron", "Mordor", "die Adler"];
-    // console.log(subject, predicate, object);
     for (let i = object.length; i <= object.length; i--) {
         if (i == 0) {
             break;
@@ -16,10 +15,11 @@ var RandomPoet_L02;
     }
     function getVerse(_subject, _predicate, _object) {
         let verse = "";
-        let calc = Math.floor(Math.random() * _subject.length);
-        verse = _subject.splice(calc, 1) + " "
-            + _predicate.splice(calc, 1) + " "
-            + _object.splice(calc, 1) + "!";
+        let nSubject = Math.floor(Math.random() * _subject.length);
+        let nPredicate = Math.floor(Math.random() * _predicate.length);
+        let nObject = Math.floor(Math.random() * _object.length);
+        verse = _subject.splice(nSubject, 1) + " " + _predicate.splice(nPredicate, 1) + " " + +_object.splice(nObject, 1) + "!";
+        // console.log(verse);
         return verse;
     }
 })(RandomPoet_L02 || (RandomPoet_L02 = {}));
--- L01/RandomPoem/L01_RandomPoem.ts
+++ L01/RandomPoem/L01_RandomPoem.ts
@@ -5,9 +5,6 @@ namespace RandomPoet_L02 {
     let predicate: string[] = ["sucht", "verzaubert", "liebt", "hasst", "verflucht", "vernichtet"];
     let object: string[] = ["den Ring", "den Schicksalsberg", "Elben", "Sauron", "Mordor", "die Adler"];
 
-
-    // console.log(subject, predicate, object);
-
     for (let i: number = object.length; i <= object.length; i--) {
         if (i == 0) {
             break;
@@ -20,12 +17,16 @@ namespace RandomPoet_L02 {
 
     function getVerse(_subject: string[], _predicate: string[], _object: string[]): string {
         let verse: string = "";
-        let calc: number = Math.floor(Math.random() * _subject.length);
 
-        verse = _subject.splice(calc, 1) + " " 
-                + _predicate.splice(calc, 1) + " " 
-                + _object.splice(calc, 1) + "!";
-       
+        let nSubject: number = Math.floor(Math.random() * _subject.length);
+        let nPredicate: number = Math.floor(Math.random() * _predicate.length);
+        let nObject: number = Math.floor(Math.random() * _object.length);
+
+        verse = _subject.splice(nSubject, 1) + " " + _predicate.splice(nPredicate, 1) + " " + +_object.splice(nObject, 1) + "!";
+
+
+
+        // console.log(verse);
         return verse;
 
     }
