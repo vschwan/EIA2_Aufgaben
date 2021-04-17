5ddf9dcd2977a0a4314f410557db88eefb4ed485
Valentina Schwan <56428408+vschwan@users.noreply.github.com>
2021-04-17 09:50:21 +0200

    another commit

--- L02/L02_EventInspector/EventInspector.js
+++ L02/L02_EventInspector/EventInspector.js
@@ -8,17 +8,43 @@ var L02_IventInspector;
         document.addEventListener("click", logInfo);
         document.body.addEventListener("click", logInfo);
         document.body.addEventListener("keyup", logInfo);
-        let div = document.getElementsByName("div");
+        //   let div = document.getElementsByName("div");
+        /*   let button: HTMLButtonElement = <HTMLButtonElement>document.querySelector("button");
+            let myEvent: CustomEvent = new CustomEvent("myevent", {
+                detail: {},
+                bubbles: true,
+                cancelable: true,
+                composed: false
+            });
+    
+            button.dispatchEvent(myEvent);
+            button.addEventListener("myevent", (_event) => {
+                console.log("im lstening on a custom event");
+            
+            });*/
     }
     function setInfoBox(_event) {
         let position = "x:" + _event.clientX + "y:" + _event.clientY;
         // let target: EventTarget = <EventTarget> _event.target;
         let span = document.querySelector("span");
-        console.log(_event.currentTarget, position);
+        //console.log(_event.currentTarget, position);
         span.innerHTML = position + _event.currentTarget;
+        console.log(span.offsetLeft, span.offsetTop);
+        span.style.left = _event.clientX + "px";
+        span.style.top = _event.clientY + "px";
     }
     function logInfo(_event) {
-        console.log(_event.currentTarget, _event.target, _event.type, _event);
+        //    console.log(_event.currentTarget, _event.target, _event.type, _event);
     }
+    /* function handleClick(_event: MouseEvent): void {
+         let target: Node = <Node>_event.target;
+         console.log(target);
+         if (target == mail) {
+             placeLetter(_event);
+         }
+         else {
+             deleteLetter(_event);
+         }
+     }*/
 })(L02_IventInspector || (L02_IventInspector = {}));
 //# sourceMappingURL=EventInspector.js.map
--- L02/L02_EventInspector/EventInspector.ts
+++ L02/L02_EventInspector/EventInspector.ts
@@ -9,7 +9,22 @@ namespace L02_IventInspector {
         document.addEventListener("click", logInfo);
         document.body.addEventListener("click", logInfo);
         document.body.addEventListener("keyup", logInfo);
-        let div = document.getElementsByName("div");
+        //   let div = document.getElementsByName("div");
+
+
+        /*   let button: HTMLButtonElement = <HTMLButtonElement>document.querySelector("button");
+            let myEvent: CustomEvent = new CustomEvent("myevent", {
+                detail: {},
+                bubbles: true,
+                cancelable: true,
+                composed: false
+            });
+    
+            button.dispatchEvent(myEvent);
+            button.addEventListener("myevent", (_event) => {
+                console.log("im lstening on a custom event");
+            
+            });*/
 
     }
 
@@ -17,12 +32,30 @@ namespace L02_IventInspector {
         let position: string = "x:" + _event.clientX + "y:" + _event.clientY;
         // let target: EventTarget = <EventTarget> _event.target;
         let span: HTMLSpanElement = <HTMLSpanElement>document.querySelector("span");
-        console.log(_event.currentTarget, position);
+        //console.log(_event.currentTarget, position);
         span.innerHTML = position + _event.currentTarget;
+
+        console.log(span.offsetLeft, span.offsetTop);
+        span.style.left = _event.clientX + "px";
+        span.style.top = _event.clientY + "px";
 
     }
 
     function logInfo(_event: Event): void {
-        console.log(_event.currentTarget, _event.target, _event.type, _event);
+        //    console.log(_event.currentTarget, _event.target, _event.type, _event);
     }
+
+
+    /* function handleClick(_event: MouseEvent): void {
+         let target: Node = <Node>_event.target;
+         console.log(target);
+         if (target == mail) {
+             placeLetter(_event);
+         }
+         else {
+             deleteLetter(_event);
+         }
+     }*/
+
+
 }
