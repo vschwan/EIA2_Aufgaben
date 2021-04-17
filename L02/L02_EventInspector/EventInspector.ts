c5d24d237166eb8f77c04c2b89453d86902e5727
Valentina Schwan <56428408+vschwan@users.noreply.github.com>
2021-04-17 11:59:57 +0200

    another commit

--- L02/L02_EventInspector/EventInspector.ts
+++ L02/L02_EventInspector/EventInspector.ts
@@ -7,33 +7,75 @@ namespace L02_IventInspector {
         document.addEventListener("mousemove", setInfoBox);
         document.addEventListener("keyup", logInfo);
         document.addEventListener("click", logInfo);
+
         document.body.addEventListener("click", logInfo);
         document.body.addEventListener("keyup", logInfo);
-        //   let div = document.getElementsByName("div");
+
+        let div1: NodeListOf<HTMLElement> = document.getElementsByName("div1");
+        for (let i: number = 0; i < div1.length; i++) {
+            div1[i].addEventListener("click", logInfo);
+        }
+
+        let div0: NodeListOf<HTMLElement> = document.getElementsByName("div0");
+        for (let i: number = 0; i < div0.length; i++) {
+            div0[i].addEventListener("click", logInfo);
+        }
 
 
-        /*   let button: HTMLButtonElement = <HTMLButtonElement>document.querySelector("button");
-            let myEvent: CustomEvent = new CustomEvent("myevent", {
-                detail: {},
-                bubbles: true,
-                cancelable: true,
-                composed: false
-            });
-    
-            button.dispatchEvent(myEvent);
-            button.addEventListener("myevent", (_event) => {
-                console.log("im lstening on a custom event");
-            
-            });*/
+        let button: HTMLButtonElement = <HTMLButtonElement>document.querySelector("button");
+        button.addEventListener("click", handleButton);
+
+
+        /*
+                let myEvent: CustomEvent = new CustomEvent("myevent", {
+                    detail: {target: () => myEvent.currentTarget},
+                    bubbles: true,
+                    cancelable: true
+                });
+        
+                document.addEventListener("myevent", e => console.log(e.detail.target()));
+                button.dispatchEvent(myEvent);
+        
+        
+        
+                button.addEventListener("myevent", function (_event: Event): void {
+                    console.log("hello");
+                });*/
+
+
+
 
     }
+
+    function handleButton(_event: Event): void {
+
+        let myEvent: CustomEvent = new CustomEvent("myevent", {
+            detail: {},
+            bubbles: true,
+            cancelable: true
+        });
+
+        let target: EventTarget = <EventTarget>_event.target;
+        console.log(target);
+        document.dispatchEvent(myEvent);
+        /*console.log(myEvent.currentTarget);
+         console.log(myEvent.eventPhase);*/
+        console.log(myEvent);
+        //console.log(target);
+    }
+
+
+
+
+
+
 
     function setInfoBox(_event: MouseEvent): void {
         let position: string = "x:" + _event.clientX + "y:" + _event.clientY;
         // let target: EventTarget = <EventTarget> _event.target;
         let span: HTMLSpanElement = <HTMLSpanElement>document.querySelector("span");
         //console.log(_event.currentTarget, position);
-        span.innerHTML = position + _event.currentTarget;
+        span.innerHTML = position + "<br>" + _event.target;
 
         console.log(span.offsetLeft, span.offsetTop);
         span.style.left = _event.clientX + "px";
@@ -42,20 +84,11 @@ namespace L02_IventInspector {
     }
 
     function logInfo(_event: Event): void {
-        //    console.log(_event.currentTarget, _event.target, _event.type, _event);
+        console.log("Current Target: " + _event.currentTarget, "Target: " + _event.target, "Event Type: " + _event.type, "Event: " + _event);
     }
 
 
-    /* function handleClick(_event: MouseEvent): void {
-         let target: Node = <Node>_event.target;
-         console.log(target);
-         if (target == mail) {
-             placeLetter(_event);
-         }
-         else {
-             deleteLetter(_event);
-         }
-     }*/
+
 
 
 }
