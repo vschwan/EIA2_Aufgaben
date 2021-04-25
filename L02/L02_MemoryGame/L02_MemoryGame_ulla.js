130ba91bc0cb5a98053542ca13e6672d8c16251d
Valentina Schwan <56428408+vschwan@users.noreply.github.com>
2021-04-25 05:47:38 +0200

    update

--- L02/L02_MemoryGame/L02_MemoryGame_ulla.js
+++ L02/L02_MemoryGame/L02_MemoryGame_ulla.js
@@ -3,7 +3,8 @@ var L02_MemoryGame;
 (function (L02_MemoryGame) {
     window.addEventListener("load", handleLoad);
     function handleLoad() {
-        let prompt = parseInt(prompt("WElcome to my Memory Game br", "16"), 11);
+        let prompt = prompt("WElcome to my Memory Game br", "16");
+        d;
     }
 })(L02_MemoryGame || (L02_MemoryGame = {}));
 //# sourceMappingURL=L02_MemoryGame_ulla.js.map
