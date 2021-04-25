21e6de2884bd3c92b8ff54396bf0a8511f6dba7c
Valentina Schwan <56428408+vschwan@users.noreply.github.com>
2021-04-25 06:48:26 +0200

    update

--- L02/L02_MemoryGame/L02_MemoryGame_ulla.js
+++ L02/L02_MemoryGame/L02_MemoryGame_ulla.js
@@ -18,8 +18,23 @@ var L02_MemoryGame;
     function getCards() {
         let amountPairs = parseInt(pairsInput);
         playingCardsOne = allCards.slice(0, amountPairs);
+        playingCardsTwo = playingCardsOne.slice(0, playingCardsOne.length);
     }
     function startTimer() {
+        // let timer = setInterval(s)
+    }
+    function displayCards(_playingCardsOne, _playingCardsTwo) {
+        for (let index = playingCardsOne.length; index > 0; index--) {
+            let x = Math.floor(Math.random() * _playingCardsOne.length);
+            let oneCard1 = _playingCardsOne.splice(x, 1);
+            let card1 = document.createElement("span");
+            document.body.appendChild(card1);
+            card1.innerHTML = oneCard1[0];
+            card1.id = card1.innerHTML;
+            card1.addEventListener("click", controlCard);
+        }
+    }
+    function controlCard() {
     }
 })(L02_MemoryGame || (L02_MemoryGame = {}));
 //# sourceMappingURL=L02_MemoryGame_ulla.js.map
--- L02/L02_MemoryGame/L02_MemoryGame_ulla.ts
+++ L02/L02_MemoryGame/L02_MemoryGame_ulla.ts
@@ -28,9 +28,33 @@ namespace L02_MemoryGame {
     function getCards(): void {
         let amountPairs: number = parseInt(pairsInput);
         playingCardsOne = allCards.slice(0, amountPairs);
+        playingCardsTwo = playingCardsOne.slice(0, playingCardsOne.length);
     }
 
     function startTimer(): void {
+        // let timer = setInterval(s)
+
+    }
+
+    function displayCards(_playingCardsOne: string[], _playingCardsTwo: string[]): void {
+
+        for (let index: number = playingCardsOne.length; index > 0; index--) {
+            let x: number = Math.floor(Math.random() * _playingCardsOne.length);
+
+            let oneCard1: string[] = _playingCardsOne.splice(x, 1);
+            let card1: HTMLSpanElement = <HTMLSpanElement>document.createElement("span");
+            document.body.appendChild(card1);
+            card1.innerHTML = oneCard1[0];
+            card1.id = card1.innerHTML;
+            card1.addEventListener("click", controlCard);
+
+
+
+
+        }
+    }
+
+    function controlCard(): void {
 
     }
 
